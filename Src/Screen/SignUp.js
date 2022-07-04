14dfc8745c0530afdaa8e5e545769a0b95ee9366
Hassam <aistrator94@gmail.com>
2022-07-04 12:46:14 +0500

    login screen

---
+++ Src/Screen/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+function SignUp(props) {
+  return (
+    <View>
+      <Text>SignUp</Text>
+    </View>
+  );
+}
+
+export default SignUp;
+
+const styles = StyleSheet.create({});
--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+
+import App from './Src/Screen/Login'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
